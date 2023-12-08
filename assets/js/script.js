fba4e6246c148457e583d89b60e9ce9c853ebfa5
rasyaadmk <rasyaadmaulanakhandiyas_1313621020@mhs.unj.ac.id>
2023-12-08 20:47:27 +0700

    About Us Finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > 100) {
-        nav.classList.add('navbg', 'shadow');
+        nav.classList.add('shadow');
     } else {
-        nav.classList.remove('navbg', 'shadow');
+        nav.classList.remove('shadow');
     }
 });
